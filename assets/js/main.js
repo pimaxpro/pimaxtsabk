8b458a40565d3e01818b278af3f54961b97ec08f
pimaxpro <dducpimax@gmail.com>
2025-12-31 16:10:18 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
+function toggleSolution(btn) {
+  const content = btn.nextElementSibling;
+  content.classList.toggle("open");
 
+  if (content.classList.contains("open")) {
+    MathJax.typesetPromise([content]);
+  }
+}
+
+const tagLinks = document.querySelectorAll('.tag');
+
+tagLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const tag = link.getAttribute('data-tag');
+    const posts = document.querySelectorAll('.post');
+    posts.forEach(post => {
+      const postTags = post.getAttribute('data-tags').split(',').map(t => t.trim());
+      post.style.display = postTags.includes(tag) ? 'block' : 'none';
+    });
+  });
+});
